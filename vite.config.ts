feeed2d770b2dbafc426f5f473882ad473338d4c
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-12-27 21:05:11 +0300

    style: format

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
+import devtools from 'solid-devtools/vite';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+    plugins: [devtools(), solidPlugin()],
+    server: {
+        port: 3000,
+    },
+    build: {
+        target: 'esnext',
+    },
 });
