5ab58817eb426aaec321ea4656664af7ed6f59c1
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-12-27 18:49:27 +0300

    feat: display list of weapons

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,54 @@ const weapons_list = weapons_list_json as Weapon[];
 const App: Component = () => {
     // const [loadout, setLoadout] = createSignal<Loadout | null>(null);
 
+    function addWeapon(hash: string) {
+        console.log(hash);
+    }
+
     return (
         <div class='container'>
             <div class='sidebar'>
                 <h1>Weapons list</h1>
                 {weapons_list.map((weapon) => {
-                    return <div class='weapon-list-item'>{weapon.name}</div>;
+                    return (
+                        <div class='weapon-list-item'>
+                            <img
+                                class='weapon-list-item-img'
+                                src={`/weapons/${weapon.hash}.png`}
+                                alt='no image!'
+                            />
+                            <div class='weapon-list-item-info'>
+                                <div class='weapon-list-item-info-text'>
+                                    <span>
+                                        <strong>Name: </strong>
+                                        {weapon.name}
+                                    </span>
+                                    <span>
+                                        <strong>Hash: </strong>
+                                        {weapon.hash}
+                                    </span>
+                                    <span>
+                                        <strong>Group: </strong>
+                                        {weapon.group}
+                                    </span>
+                                    <span>
+                                        <strong>DLC: </strong>
+                                        {weapon.dlc}
+                                    </span>
+                                    <span>
+                                        <strong>Description: </strong>
+                                        {weapon.description}
+                                    </span>
+                                </div>
+                            </div>
+                            <button
+                                onClick={() => addWeapon(weapon.hash)}
+                                class='weapon-list-item-add-btn'
+                            >
+                                Add
+                            </button>
+                        </div>
+                    );
                 })}
             </div>
             <div class='main-content'>
