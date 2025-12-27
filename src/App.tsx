ecad82b346f52e6b2f92a30fc6c54ecc5fd3a6b6
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-12-27 19:00:54 +0300

    feat: add weapon to loadout

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,42 @@ import weapons_list_json from './weapons.json';
 const weapons_list = weapons_list_json as Weapon[];
 
 const App: Component = () => {
-    // const [loadout, setLoadout] = createSignal<Loadout | null>(null);
+    const [loadout, setLoadout] = createSignal<Loadout | null>(null);
 
     function addWeapon(hash: string) {
-        console.log(hash);
+        if (loadout() === null) {
+            setLoadout({
+                name: 'New Loadout',
+                weapons: [],
+            });
+        }
+
+        const weapon = weapons_list.find((w) => w.hash === hash);
+
+        if (
+            weapon === undefined ||
+            loadout()!.weapons.some((w) => w.hash === hash)
+        ) {
+            return;
+        }
+
+        const new_weapon: LoadoutWeapon = {
+            name: weapon.name,
+            hash: weapon.hash,
+            model_hash_key: weapon.model_hash_key,
+            group: weapon.group,
+            display_name: null,
+            starting_ammo_count: null,
+            is_vehicle_weapon: null,
+            use_racking_animation: null,
+            weapon_location: null,
+            components: [],
+            tint: null,
+        };
+
+        setLoadout({
+            weapons: [...loadout()!.weapons, new_weapon],
+        });
     }
 
     return (
