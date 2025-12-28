c64aa84bcf8f1b2bf16f8cdfb782a7bc486932a7
Abdalrzag Eisa <bmjfdrh@gmail.com>
2025-12-28 13:49:27 +0300

    feat: display loadout weapons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Component, createMemo, createSignal, For } from 'solid-js';
+import { Component, createMemo, createSignal, For, Show } from 'solid-js';
 import type { Loadout, LoadoutWeapon, Weapon } from './consts';
 import './app.css';
 
@@ -33,6 +33,71 @@ const App: Component = () => {
 
     function clearLoadout() {
         setLoadout(null);
+    }
+
+    function changeWeaponDisplayname(value: string, hash: string) {
+        if (loadout() === null) return;
+
+        const index = loadout()!.weapons.findIndex((w) => w.hash === hash);
+
+        if (index !== -1) {
+            loadout()!.weapons[index].display_name = value;
+        }
+    }
+
+    function changeWeaponStartingAmmoCount(value: string, hash: string) {
+        if (loadout() === null) return;
+
+        const index = loadout()!.weapons.findIndex((w) => w.hash === hash);
+
+        if (index !== -1) {
+            loadout()!.weapons[index].starting_ammo_count = parseInt(value);
+        }
+    }
+
+    function changeWeaponIsVehicleWeapon(value: boolean, hash: string) {
+        if (loadout() === null) return;
+
+        const index = loadout()!.weapons.findIndex((w) => w.hash === hash);
+
+        if (index !== -1) {
+            loadout()!.weapons[index].is_vehicle_weapon = value;
+        }
+    }
+
+    function changeWeaponUseRackingAnimation(value: boolean, hash: string) {
+        if (loadout() === null) return;
+
+        const index = loadout()!.weapons.findIndex((w) => w.hash === hash);
+
+        if (index !== -1) {
+            loadout()!.weapons[index].use_racking_animation = value;
+        }
+    }
+
+    function clearWeapon(hash: string) {
+        if (loadout() === null) return;
+
+        const index = loadout()!.weapons.findIndex((w) => w.hash === hash);
+
+        if (index !== -1) {
+            loadout()!.weapons[index].display_name = null;
+            loadout()!.weapons[index].starting_ammo_count = null;
+            loadout()!.weapons[index].is_vehicle_weapon = null;
+            loadout()!.weapons[index].use_racking_animation = null;
+            loadout()!.weapons[index].weapon_location = null;
+            loadout()!.weapons[index].components = [];
+            loadout()!.weapons[index].tint = null;
+        }
+    }
+
+    function removeWeapon(hash: string) {
+        if (loadout() === null) return;
+
+        setLoadout({
+            name: loadout()!.name,
+            weapons: loadout()!.weapons.filter((w) => w.hash !== hash),
+        });
     }
 
     function addWeapon(hash: string) {
@@ -156,13 +221,152 @@ const App: Component = () => {
                         disabled={loadout() === null}
                     />
                     <div class='main-content-navbar-buttons'>
-                        <button onClick={() => clearLoadout()}>
+                        <button
+                            onClick={() => clearLoadout()}
+                            disabled={loadout() === null}
+                        >
                             Clear loadout
                         </button>
                         <button disabled={true}>Load loadout</button>
                         <button disabled={true}>Export loadout</button>
                     </div>
                 </div>
+
+                <Show when={loadout() !== null}>
+                    <div class='loadout-weapons-list'>
+                        <For each={loadout()?.weapons}>
+                            {(weapon) => (
+                                <div class='loadout-weapon-list-item'>
+                                    <img
+                                        class='loadout-weapon-list-item-img'
+                                        src={`/weapons/${weapon.hash}.png`}
+                                        alt='no image!'
+                                    />
+                                    <div class='loadout-weapon-list-item-info'>
+                                        <div class='loadout-weapon-list-item-info-text'>
+                                            <span>
+                                                <strong>Name: </strong>
+                                                {weapon.name}
+                                            </span>
+                                            <span>
+                                                <strong>Hash: </strong>
+                                                {weapon.hash}
+                                            </span>
+                                            <span>
+                                                <strong>Group: </strong>
+                                                {weapon.group}
+                                            </span>
+                                            <div class='loadout-weapon-list-item-settings-list'>
+                                                <input
+                                                    placeholder='Display name'
+                                                    value={
+                                                        weapon.display_name ||
+                                                        ''
+                                                    }
+                                                    onChange={(e) =>
+                                                        changeWeaponDisplayname(
+                                                            e.target.value,
+                                                            weapon.hash,
+                                                        )
+                                                    }
+                                                    disabled={
+                                                        weapon.is_vehicle_weapon !==
+                                                        true
+                                                    }
+                                                />
+                                                <div class='loadout-weapon-list-item-setting-input-lable'>
+                                                    <span>
+                                                        Starting ammo count
+                                                    </span>
+                                                    <input
+                                                        class='loadout-weapon-ammo-count-input'
+                                                        type='number'
+                                                        max={9999}
+                                                        value={
+                                                            weapon.starting_ammo_count ||
+                                                            0
+                                                        }
+                                                        onChange={(e) =>
+                                                            changeWeaponStartingAmmoCount(
+                                                                e.target.value,
+                                                                weapon.hash,
+                                                            )
+                                                        }
+                                                    />
+                                                </div>
+                                                <div class='loadout-weapon-list-item-setting-input-lable'>
+                                                    <span>
+                                                        is vehicle weapon
+                                                    </span>
+                                                    <input
+                                                        type='checkbox'
+                                                        checked={
+                                                            weapon.is_vehicle_weapon ||
+                                                            false
+                                                        }
+                                                        onChange={(e) =>
+                                                            changeWeaponIsVehicleWeapon(
+                                                                e.target
+                                                                    .checked,
+                                                                weapon.hash,
+                                                            )
+                                                        }
+                                                    />
+                                                </div>
+                                                <div class='loadout-weapon-list-item-setting-input-lable'>
+                                                    <span>
+                                                        use racking animation
+                                                    </span>
+                                                    <input
+                                                        type='checkbox'
+                                                        checked={
+                                                            weapon.use_racking_animation ||
+                                                            false
+                                                        }
+                                                        onChange={(e) =>
+                                                            changeWeaponUseRackingAnimation(
+                                                                e.target
+                                                                    .checked,
+                                                                weapon.hash,
+                                                            )
+                                                        }
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div class='loadout-weapon-list-item-setting-input-lable'>
+                                                <span>weapon_location</span>
+                                            </div>
+                                            <div class='loadout-weapon-list-item-setting-input-lable'>
+                                                <span>weapon_location</span>
+                                            </div>
+                                            <div class='loadout-weapon-list-item-setting-input-lable'>
+                                                <span>components</span>
+                                            </div>
+                                            <div class='loadout-weapon-list-item-setting-input-lable'>
+                                                <span>tint</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <button
+                                        onClick={() => clearWeapon(weapon.hash)}
+                                        class='weapon-list-item-remove-btn'
+                                        title='Clear weapon properties'
+                                    >
+                                        clear
+                                    </button>
+                                    <button
+                                        onClick={() =>
+                                            removeWeapon(weapon.hash)
+                                        }
+                                        class='weapon-list-item-remove-btn'
+                                    >
+                                        Remove
+                                    </button>
+                                </div>
+                            )}
+                        </For>
+                    </div>
+                </Show>
             </div>
         </div>
     );
